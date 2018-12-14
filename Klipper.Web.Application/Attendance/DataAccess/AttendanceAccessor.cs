d9ed0c7f65a4e61a95c1e59cdcb4e5e7763c46f7
nakwarsi <nakwarsi@hotmail.com>
2018-12-14 17:57:07 +0530

    return the Attendance for start and End Time

---
+++ Klipper.Web.Application/Attendance/DataAccess/AttendanceAccessor.cs
@@ -0,0 +1,36 @@
+﻿using Models.Core.HR.Attendance;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klipper.Web.Application.Attendance.DataAccess
+{
+    public class AttendanceAccessor : IAttendanceAccessor
+    {
+        static HttpClient _client2 = new HttpClient() { Timeout = TimeSpan.FromMilliseconds(10000) };
+        static string _baseAddress2 = "http://localhost:7000/";
+        public IEnumerable<AccessEvent> GetAttendanceByDateIDAsync(int employeeId, DateTime startDate, DateTime endDate)
+        {
+            _client2.BaseAddress = new Uri(_baseAddress2);
+            _client2.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var startStr = startDate.Year.ToString() + "-" + startDate.Month.ToString() + "-" + startDate.Day.ToString();
+            var endStr = endDate.Year.ToString() + "-" + endDate.Month.ToString() + "-" + endDate.Day.ToString();
+            var str = "api/attendance/" + employeeId.ToString() + "/" + startStr + "/" + endStr;
+            HttpResponseMessage response = _client2.GetAsync(str).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                var accessEvents = JsonConvert.DeserializeObject<IEnumerable<AccessEvent>>(jsonString);
+                return accessEvents;
+            }
+            else
+            {
+                return new List<AccessEvent>();
+            }
+        }
+    }
+}
---
+++ Klipper.Web.Application/Attendance/Service/AttendanceService.cs
@@ -0,0 +1,27 @@
+﻿using Klipper.Web.Application.Attendance.DataAccess;
+using Models.Core.HR.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klipper.Web.Application.Attendance.Service
+{
+    public class AttendanceService : IAttendanceService
+    {
+        private IAttendanceAccessor _attendanceAccessor;
+
+        public AttendanceService(IAttendanceAccessor attendanceAccessor)
+        {
+            _attendanceAccessor = attendanceAccessor;
+        }
+
+        public IEnumerable<AccessEvent> GetAttendanceByDate(int employeeId, DateTime startDate, DateTime endDate)
+        {
+            var accessEvents = _attendanceAccessor.GetAttendanceByDateIDAsync(employeeId, startDate, endDate) as List<AccessEvent>;
+            var distinctDay = accessEvents.Select(x => x.EventTime.Date).Distinct();
+            return accessEvents;
+        }
+    }
+}
