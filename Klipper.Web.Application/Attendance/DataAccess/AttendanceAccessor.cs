70a0f050aec48451f075eedb2bd940d8da7ae457
nakwarsi <nakwarsi@hotmail.com>
2018-12-14 18:10:48 +0530

    service for returning attendance for specific date is added in application layer

--- Klipper.Web.Application/Attendance/DataAccess/AttendanceAccessor.cs
+++ Klipper.Web.Application/Attendance/DataAccess/AttendanceAccessor.cs
@@ -1,4 +1,5 @@
-﻿using Models.Core.HR.Attendance;
+﻿using Common;
+using Models.Core.HR.Attendance;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -11,16 +12,13 @@ namespace Klipper.Web.Application.Attendance.DataAccess
 {
     public class AttendanceAccessor : IAttendanceAccessor
     {
-        static HttpClient _client2 = new HttpClient() { Timeout = TimeSpan.FromMilliseconds(10000) };
-        static string _baseAddress2 = "http://localhost:7000/";
         public IEnumerable<AccessEvent> GetAttendanceByDateIDAsync(int employeeId, DateTime startDate, DateTime endDate)
         {
-            _client2.BaseAddress = new Uri(_baseAddress2);
-            _client2.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var client = CommonHelper.GetClient(AddressResolver.GetAddress("KlipperApi", false));
             var startStr = startDate.Year.ToString() + "-" + startDate.Month.ToString() + "-" + startDate.Day.ToString();
             var endStr = endDate.Year.ToString() + "-" + endDate.Month.ToString() + "-" + endDate.Day.ToString();
             var str = "api/attendance/" + employeeId.ToString() + "/" + startStr + "/" + endStr;
-            HttpResponseMessage response = _client2.GetAsync(str).Result;
+            HttpResponseMessage response = client.GetAsync(str).Result;
             if (response.IsSuccessStatusCode)
             {
                 var jsonString = response.Content.ReadAsStringAsync().Result;
