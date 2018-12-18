fc1d74c85b2e5dbd1262f7b39fc77a9b78e82af8
Vadg_Sid <praD1737>
2018-12-18 16:52:00 +0530

    Added Service to get the reportees Data in Klipper gateway

---
+++ KlipperApi/Controllers/Reportee/ReporteesAccessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using KlipperApi.Controllers.Auth;
+using Models.Core.Employment;
+using Models.Core.HR.Attendance;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KlipperApi.Controllers.Reportee
+{
+    public class ReporteesAccessor : IReporteesAccessor
+    {
+        private readonly IUserRepository _userRepository = null;
+
+
+        public ReporteesAccessor(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        public async Task<List<Employee>> GetReporteesByEmployeeIDAsync(int employeeId)
+        {
+            var employeeApiClient = CommonHelper.GetClient(AddressResolver.GetAddress("EmployeeApi", false));
+            var employeeApiString = "api/employees/" + employeeId.ToString();
+            HttpResponseMessage responseForEmployeeApi = await employeeApiClient.GetAsync(employeeApiString);
+            var jsonStringForEmployeeApi = await responseForEmployeeApi.Content.ReadAsStringAsync();
+
+            var jsonObject = JObject.Parse(jsonStringForEmployeeApi);
+            string reportees = jsonObject["reportees"].ToString();
+
+            JArray totalReportees = (JArray)jsonObject["reportees"];
+
+           List<Employee> dataOfReportees = new List<Employee>();
+
+            for (int i =0;i<totalReportees.Count;i++)
+            {
+                var idOfReporteeFromTotalReportees = totalReportees[i].Value<int>();
+                var empApiString = "api/employees/" + idOfReporteeFromTotalReportees.ToString();
+                HttpResponseMessage responseForAttendanceApi = await employeeApiClient.GetAsync(empApiString);
+                var jsonStrigForEmpApi = await responseForAttendanceApi.Content.ReadAsStringAsync();
+                var employeeData = JsonConvert.DeserializeObject<Employee>(jsonStrigForEmpApi);
+                dataOfReportees.Add(employeeData);
+            }
+
+            return dataOfReportees;
+        }
+    }
+}
---
+++ KlipperApi/Controllers/Reportee/ReporteesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Core.Employment;
+using Models.Core.HR.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlipperApi.Controllers.Reportee
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReporteesController : ControllerBase
+    {
+        private IReporteesAccessor _reporteesAccessor;
+
+
+        public ReporteesController(IReporteesAccessor reporteesAccessor)
+        {
+            _reporteesAccessor = reporteesAccessor;
+        }
+
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByEmployeeId(int employeeId)
+        {
+            if (employeeId < 0)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var reprteed = await _reporteesAccessor.GetReporteesByEmployeeIDAsync(employeeId) as List<Employee>;
+
+            if (reprteed == null)
+            {
+                return NotFound();
+            }
+            return Ok(reprteed);
+        }
+    }
+}
