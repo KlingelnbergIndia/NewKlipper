197672f574255fc40e2d90d4ef4338b862b31651
Naik_San <snketnaikredkar@gmail.com>
2018-12-28 10:55:11 +0530

    conflicts resolved

---
+++ KlipperAuth.Service/UseCases/AuthenticateUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlipperAuth.Service.UseCases
+{
+    public class AuthenticateUser
+    {
+        private IUserRepository _userRepository;
+        public AuthenticateUser(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+    }
+}
