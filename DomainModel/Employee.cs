a6241afccb5c46c47fc8b2ddb86a55a00eb41b1d
Naik_San <snketnaikredkar@gmail.com>
2019-01-21 15:03:58 +0530

    Departments added to Employee entity

--- DomainModel/Employee.cs
+++ DomainModel/Employee.cs
@@ -16,6 +16,21 @@ namespace DomainModel
         TeamLeader
     }
 
+    public enum Departments
+    {
+        Default,
+        Software,
+        Design,
+        Service,
+        Admin,
+        Finance,
+        TechCentre,
+        AfterSales,
+        Projects,
+        Sales,
+        CompetenceCentre
+    }
+
     public class Employee
     {
         private int _id;
@@ -26,8 +41,10 @@ namespace DomainModel
         private string _title;
         private List<EmployeeRoles> _roles;
         private List<int> _reportees;
+        private Departments _department;
 
-        public Employee(int id, string userName, string password, string firstName, string lastName,string title, List<EmployeeRoles> roles, List<int> reportees)
+
+        public Employee(int id, string userName, string password, string firstName, string lastName,string title, List<EmployeeRoles> roles, List<int> reportees, Departments department)
         {
             _id = id;
             _userName = userName;
@@ -37,6 +54,7 @@ namespace DomainModel
             _roles = roles;
             _title = title;
             _reportees = reportees;
+            _department = department;
         }
 
         public int Id()
