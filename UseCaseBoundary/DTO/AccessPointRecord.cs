ea234d816da45847729fb7f0792aa39a82a5e56e
Naik_San <snketnaikredkar@gmail.com>
2019-01-22 14:19:25 +0530

    AccessPointsRecordsDto added from shweta's code

---
+++ UseCaseBoundary/DTO/AccessPointRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UseCaseBoundary.Model;
+
+namespace UseCaseBoundary.DTO
+{
+    public class AccessPointRecord
+    {
+        public Time TimeIn;
+        public Time TimeOut;
+        public Time TimeSpend;
+        public string AccessPoint;
+    }
+}
