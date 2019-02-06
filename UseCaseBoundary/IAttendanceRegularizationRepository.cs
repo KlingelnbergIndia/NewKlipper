4ceee93faaa6b0f09a39f45cc683fdf9b867bc16
Naik_San <snketnaikredkar@gmail.com>
2019-02-06 17:23:13 +0530

    SaveRegularizationData() added to interface

--- UseCaseBoundary/IAttendanceRegularizationRepository.cs
+++ UseCaseBoundary/IAttendanceRegularizationRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UseCaseBoundary.DTO;
 
 namespace UseCaseBoundary
 {
     public interface IAttendanceRegularizationRepository
     {
+        ReguralizationDTO SaveRegularizationData(ReguralizationDTO regularizationData);
     }
 }
