d0b5ce2b342285ac1950b174d8c01e964543daa1
Naik_San <snketnaikredkar@gmail.com>
2019-02-14 17:23:29 +0530

    code formatted

--- DataAccess/EntityModel/Leave/CarryForwardLeavesEntityModel.cs
+++ DataAccess/EntityModel/Leave/CarryForwardLeavesEntityModel.cs
@@ -16,9 +16,13 @@ namespace DataAccess.EntityModel.Leave
         public int EmployeeId { get; set; }
         [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime LeaveBalanceTillDate { get; set; }
+
+
         public int TakenCasualLeaves { get; set; }
         public int TakenSickLeaves { get; set; }
         public int TakenCompoffLeaves { get; set; }
+
+
         public int MaxCasualLeaves { get; set; }
         public int MaxSickLeaves { get; set; }
         public int MaxCompoffLeaves { get; set; }
