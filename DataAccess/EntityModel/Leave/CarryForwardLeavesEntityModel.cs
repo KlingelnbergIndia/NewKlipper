af9b97ba64a9490713e6b48aa9e44bfb6e581a95
Naik_San <snketnaikredkar@gmail.com>
2019-02-14 17:23:04 +0530

    db entity fields renamed

--- DataAccess/EntityModel/Leave/CarryForwardLeavesEntityModel.cs
+++ DataAccess/EntityModel/Leave/CarryForwardLeavesEntityModel.cs
@@ -16,8 +16,11 @@ namespace DataAccess.EntityModel.Leave
         public int EmployeeId { get; set; }
         [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime LeaveBalanceTillDate { get; set; }
-        public int RemainingEarnedLeaves { get; set; }
-        public int RemainingCasualLeaves { get; set; }
-        public int RemainingSickLeaves { get; set; }
+        public int TakenCasualLeaves { get; set; }
+        public int TakenSickLeaves { get; set; }
+        public int TakenCompoffLeaves { get; set; }
+        public int MaxCasualLeaves { get; set; }
+        public int MaxSickLeaves { get; set; }
+        public int MaxCompoffLeaves { get; set; }
     }
 }
