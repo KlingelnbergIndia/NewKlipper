c0409d174d2de8a7c0f36b2c37132232681ffa11
Jain_Shw <Jain_Shw@KLIN0068>
2019-02-22 19:20:11 +0530

    some changes for upadte leave ui

--- Application.Web/wwwroot/js/common.Leave.js
+++ Application.Web/wwwroot/js/common.Leave.js
@@ -20,3 +20,25 @@ function CancelLeave(leaveId) {
         });
     }
 }
+
+//Update leave
+function CancelLeave(leaveId) {
+    var controllerName = "Leave";
+    var actionName = "UpdateLeave";
+    var newURL = window.location.protocol + "//" + window.location.host + "/" + controllerName + "/" + actionName;
+
+    if (confirm('Do you want to really cancel this leave ?')) {
+        $.ajax({
+            type: "POST",
+            url: newURL,
+            data: {
+                LeaveId: leaveId
+            },
+            dataType: "html",
+            success: function (response) {
+                alert('Leave canceled !');
+                window.location.reload();
+            }
+        });
+    }
+}
