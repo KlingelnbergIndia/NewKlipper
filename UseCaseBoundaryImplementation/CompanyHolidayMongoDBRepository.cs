ee2760e320a14e4d14a866bd5827f04cb795b6cd
Jain_Shw <Jain_Shw@KLIN0068>
2019-03-28 18:02:54 +0530

    added repository for comapany holiday

---
+++ UseCaseBoundaryImplementation/CompanyHolidayMongoDBRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryImplementation
+{
+    public class CompanyHolidayMongoDBRepository
+    {
+        private readonly AttendanceDBContext _context = null;
+
+        public CompanyHolidayMongoDBRepository()
+        {
+            _context = AttendanceDBContext.Instance;
+        }
+
+    }
+}
