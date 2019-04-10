eb3f02159e00f9c9ee879a67fae063dc4559861f
Jain_Shw <Jain_Shw@KLIN0068>
2019-04-10 12:38:21 +0530

    change session timeout

--- Application.Web/Startup.cs
+++ Application.Web/Startup.cs
@@ -35,7 +35,7 @@ namespace Application.Web
             });
             services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromMinutes(5);
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
             });
 
             services.AddTransient<IEmployeeRepository, EmployeeMongoDBRepository>();
